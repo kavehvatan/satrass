94f5686bb4276a3b3508be5a0294fd5f7b04c312
Kaveh <kaveh@MacBook-Pro.local>
2025-08-13 23:45:23 +0330

    Full project overwrite with latest changes

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+// pages/contact.js
+import ContactHero from "../components/ContactHero";
+
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen">
+      <ContactHero />
+    </main>
+  );
+}

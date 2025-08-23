3d6c3d003e828c0daca680a795f04e962b22ae43
Kaveh <softernet@gmail.com>
2025-08-23 15:11:10 +0330

    Full project overwrite with latest changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,13 @@
 // pages/_app.js
-import '../styles/globals.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { useRouter } from 'next/router';
-
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  return (
-    <div dir="rtl" lang="fa">
-      <Header />
-      <Component {...pageProps} />
-      {/* فوتر سراسری را همه‌جا نشان بده، به جز صفحهٔ خانه */}
-      {router.pathname !== '/' && <Footer />}
-    </div>
-  );
-}
-
-// pages/_app.js
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const showFooter = router.pathname !== "/"; // فوتر همه‌جا به‌جز خانه
+
   return (
     <div dir="rtl" lang="fa" className="min-h-[100svh] flex flex-col">
       <Header />
@@ -32,10 +17,12 @@ export default function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </main>
 
-      {/* فوتر سراسری؛ به کف ستون می‌چسبد */}
-      <div className="mt-auto">
-        <Footer />
-      </div>
+      {/* فوتر سراسری؛ اگر صفحهٔ خانه نیست */}
+      {showFooter && (
+        <div className="mt-auto">
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
